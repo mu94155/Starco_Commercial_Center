474b0807bc51196f0b9672dff5264bab0ac05995
Salah Al Shanfari <sssshanfari@yahoo.com>
2025-02-10 22:43:20 +0400

    a change with simple comment in Main class

--- starco-commercial_center/src/main/java/com/aqdar/construction/finance/Main.java
+++ starco-commercial_center/src/main/java/com/aqdar/construction/finance/Main.java
@@ -3,6 +3,7 @@ package com.aqdar.construction.finance;
 import javax.swing.SwingUtilities;
 
 public class Main {
+    // this is only a test comment.
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             WelcomePage2 welcomePage2 = new WelcomePage2();
